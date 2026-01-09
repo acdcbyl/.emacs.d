2cb773cc38a315d64262d6d703c17b57592ac7b7
Aiser <2912778691@qq.com>
2026-01-09 17:24:36 +0800

    修复一些问题

--- early-init.el
+++ early-init.el
@@ -16,11 +16,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Startup speed, annoyance suppression
-(setq gc-cons-threshold 10000000)
-;; Adjust the buffer size for reading data from processes
-;; Check the maximum pipe size supported by the system `cat /proc/sys/fs/pipe-max-size`
-;; 1048576 --1MB; 4194304 --4MB ; 8388608 --8MB 
-(setq read-process-output-max (* 1 1024 1024)) ;1MB
+(setq gc-cons-threshold most-positive-fixnum)
 (setq byte-compile-warnings '(not obsolete))
 (setq warning-suppress-log-types '((comp) (bytecomp)))
 (setq native-comp-async-report-warnings-errors 'silent)
@@ -29,6 +25,7 @@
 (setq inhibit-startup-echo-area-message (user-login-name))
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (setq frame-resize-pixelwise t)
+(setq frame-inhibit-implied-resize t)
 (tool-bar-mode -1) ; All these tools are in the menu-bar anyway
 ;; Disable the menu bar
 (scroll-bar-mode -1)
