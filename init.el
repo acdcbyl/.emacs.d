a92d157b41fb62f857bec7927dd0322630571d1b
Aiser <2912778691@qq.com>
2026-06-08 18:11:56 +0800

    添加rimel作为输入法

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
 (let ((dir (locate-user-emacs-file "lisp")))
   (add-to-list 'load-path (file-name-as-directory dir))
   (add-to-list 'load-path (file-name-as-directory (expand-file-name "lang" dir))))
-; (add-to-list 'load-path "/home/aiser/.emacs.d/lisp")
+                                        ; (add-to-list 'load-path "/home/aiser/.emacs.d/lisp")
 
 (require 'init-base)
 (require 'init-icons)
@@ -84,6 +84,7 @@
 (require 'init-feed)
 ;; (require 'init-tramp-rpc)
 ;; (require 'tramp-rpc)
+(require 'init-rimel)
 (require 'init-workspaces)
 (require 'init-org)
 (require 'init-evil)
---
+++ lisp/init-rimel.el
@@ -0,0 +1,31 @@
+;;; init-rimel.el --- Bring better Chinese input method -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+;; A emacs dynamic module provide librime bindings for emacs
+(use-package
+  liberime
+  :ensure t
+  :defer t
+  :init
+  (setq liberime-auto-build t))
+
+(use-package
+  rimel
+  :ensure t
+  :defer t
+  :init (setq default-input-method "rimel")
+  :config
+  (setq rimel-disable-predicates
+        '(rimel-predicate-prog-in-code-p
+          rimel-predicate-after-alphabet-char-p
+          rimel-predicate-current-uppercase-letter-p
+          rimel-predicate-evil-mode-p))
+  (add-to-list 'rimel-disable-predicates 'rimel-predicate-org-in-src-block-p)
+  )
+
+(provide 'init-rimel)
+;;; init-rimel.el ends here
