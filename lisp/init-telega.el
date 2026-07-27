c2394ad73f57e73a03d01d9ccb5ed1f6fbe9af91
Aiser <2912778691@qq.com>
2026-07-27 22:20:21 +0800

    对telega 进行美化

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -59,7 +59,10 @@
   (setq telega-root-buffer-name "*Telega Root*")
 
   ;; Hide chat folder icon prefixes for clean layout
-  (setq telega-chat-folders-insexp (lambda () nil))
+  (setopt telega-chat-folders-insexp (lambda () nil))
+  (setopt telega-root-fill-column 70)
+  (setopt telega-filters-custom nil)
+  (setopt telega-filter-custom-show-folders nil)
 
   ;; Filter tabs at the top of root view
   (setq telega-filters-custom
@@ -73,6 +76,41 @@
   ;; Use native emoji fonts instead of PNG image files for maximum performance
   (setq telega-emoji-use-images nil)
 
+  (setq telega-builtin-palettes-alist
+        '((light
+           ((:outline "#b4637a") (:foreground "#b4637a") (:background "#d1c7c7"))
+           ((:outline "#ea9d34") (:foreground "#ea9d34") (:background "#d1ccc7"))
+           ((:outline "#907aa9") (:foreground "#907aa9") (:background "#cdc7d1"))
+           ((:outline "#568D68") (:foreground "#568D68") (:background "#c7d1c7"))
+           ((:outline "#286983") (:foreground "#286983") (:background "#cacfcf"))
+           ((:outline "#56949f") (:foreground "#56949f") (:background "#c6cbd1"))
+           ((:outline "#d7827e") (:foreground "#d7827e") (:background "#d1c7cb")))
+          (dark
+           ((:outline "#eb6f92") (:foreground "#eb6f92") (:background "#3d2828"))
+           ((:outline "#f6c177") (:foreground "#f6c177") (:background "#2c2620"))
+           ((:outline "#b294bb") (:foreground "#b294bb") (:background "#2e1e2e"))
+           ((:outline "#95b1ac") (:foreground "#95b1ac") (:background "#1e2e1e"))
+           ((:outline "#81a2be") (:foreground "#81a2be") (:background "#234242"))
+           ((:outline "#9ccfd8") (:foreground "#9ccfd8") (:background "#1e262e"))
+           ((:outline "#ebbcba") (:foreground "#ebbcba") (:background "#470528")))))
+
+  (with-eval-after-load 'telega
+    (custom-set-faces
+     '(telega-palette-builtin-green
+       ((t (:foreground "#95b1ac"
+                        :background "#1e2e1e"))))
+
+     '(telega-palette-builtin-blue
+       ((t (:foreground "#81a2be"
+                        :background "#234242"))))
+
+     '(telega-palette-builtin-orange
+       ((t (:foreground "#f6c177"
+                        :background "#2c2620"))))
+
+     '(telega-palette-builtin-purple
+       ((t (:foreground "#b294bb"
+                        :background "#2e1e2e"))))))
   :config
   ;; Enable desktop notifications
   (telega-notifications-mode 1)
@@ -102,20 +140,26 @@
   ;; Symbol and Icon Customizations (using nerd-icons)
   ;; ========================================================================
   (when (require 'nerd-icons nil t)
-    (setq telega-symbol-button-close (nerd-icons-mdicon "nf-md-close_box_outline")
-          telega-symbol-verified (nerd-icons-codicon "nf-cod-verified_filled" :face 'telega-blue)
-          telega-symbol-vertical-bar "│" ;; U+2502 Box Drawings Light Vertical
-          telega-symbol-saved-messages-tag-end (nerd-icons-faicon "nf-fa-tag")
-          telega-symbol-forum (nerd-icons-mdicon "nf-md-format_list_text")
-          telega-symbol-flames (nerd-icons-mdicon "nf-md-delete_clock")
-          telega-symbol-mark (propertize " " 'face 'telega-button-highlight)
-          telega-symbol-reply (nerd-icons-faicon "nf-fa-reply" :face 'ansi-color-blue)
-          telega-symbol-reply-quote (nerd-icons-faicon "nf-fa-reply_all" :face 'ansi-color-blue)
-          telega-symbol-forward (nerd-icons-faicon "nf-fa-mail_forward")
-          telega-symbol-checkmark (nerd-icons-mdicon "nf-md-check")
-          telega-symbol-heavy-checkmark (nerd-icons-codicon "nf-cod-check_all")
-          telega-symbol-summarize-in (nerd-icons-octicon "nf-oct-fold")
-          telega-symbol-summarize-out (nerd-icons-octicon "nf-oct-unfold")))
+    (setopt
+     telega-symbols-emojify
+     (cl-reduce (lambda (emojify key)
+                  (assq-delete-all key emojify))
+                '(verified vertical-bar checkmark forum heavy-checkmark reply reply-quote horizontal-bar forward button-close summarize-in summarize-out)
+                :initial-value telega-symbols-emojify)
+     telega-symbol-button-close (nerd-icons-mdicon "nf-md-close_box_outline")
+     telega-symbol-verified (nerd-icons-codicon "nf-cod-verified_filled")
+     telega-symbol-vertical-bar "│" ;; U+2502 Box Drawings Light Vertical
+     telega-symbol-saved-messages-tag-end (nerd-icons-faicon "nf-fa-tag")
+     telega-symbol-forum (nerd-icons-mdicon "nf-md-format_list_text")
+     telega-symbol-flames (nerd-icons-mdicon "nf-md-delete_clock")
+     telega-symbol-mark (propertize " " 'face 'telega-button-highlight)
+     telega-symbol-reply (nerd-icons-faicon "nf-fa-reply")
+     telega-symbol-reply-quote (nerd-icons-faicon "nf-fa-reply_all")
+     telega-symbol-forward (nerd-icons-faicon "nf-fa-mail_forward")
+     telega-symbol-checkmark (nerd-icons-mdicon "nf-md-check")
+     telega-symbol-heavy-checkmark (nerd-icons-codicon "nf-cod-check_all")
+     telega-symbol-summarize-in (nerd-icons-octicon "nf-oct-fold")
+     telega-symbol-summarize-out (nerd-icons-octicon "nf-oct-unfold")))
 
   ;; ;; ========================================================================
   ;; ;; Keybindings
