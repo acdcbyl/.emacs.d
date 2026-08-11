84d1664722ee5ca3622052ff154da4a728cf24dc
Aiser <2912778691@qq.com>
2026-08-11 19:11:02 +0800

    添加连字特性

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -185,19 +185,11 @@ If the new path's directories does not exist, create them."
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '("PragmataPro")
+    (cl-loop for font in '("PragmataPro Mono Liga")
              when (font-available-p font)
              return (set-face-attribute 'default nil
                                         :family font
                                         :height 140))
-    ;; Set mode-line font
-    ;; (cl-loop for font in '("SF Mono" "Menlo" "SF Pro Display" "Helvetica")
-    ;;          when (font-available-p font)
-    ;;          return (progn
-    ;;                  (set-face-attribute 'mode-line nil :family font :height 120)
-    ;;                  (when (facep 'mode-line-active)
-    ;;                    (set-face-attribute 'mode-line-active nil :family font :height 120))
-    ;;                  (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
 
     ;; Specify font for all unicode characters
     (cl-loop for font in '("Material Symbols Rounded" "Symbola" "Apple Symbols" "Segoe UI Symbol"  "Symbol")
@@ -216,6 +208,14 @@ If the new path's directories does not exist, create them."
              return (progn
                       (setq face-font-rescale-alist `((,font . 1.05)))
                       (set-fontset-font t 'han (font-spec :family font))))
+
+    ;; If your font supports ligatures, uncomment it
+    (cl-loop for chars in '("::" "..." "->" "=>" "<=" ">=" "!==" "!=" "===" "==")
+             for key = (aref chars 0)
+             do (set-char-table-range
+                 composition-function-table  key
+                 (nconc (char-table-range composition-function-table key)
+                        `(,(vector (regexp-quote chars) 0 'font-shape-gstring)))))
     ))
 
 (add-hook 'window-setup-hook #'setup-fonts)
